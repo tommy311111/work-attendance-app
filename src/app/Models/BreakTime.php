fe108584609c94bcbccf3a031b431db3dd6d9d1a
y_has <yuko1640@gmail.com>
2025-07-14 14:49:29 +0900

    BreakTimeモデルの中身作成

--- src/app/Models/BreakTime.php
+++ src/app/Models/BreakTime.php
@@ -8,4 +8,24 @@ use Illuminate\Database\Eloquent\Model;
 class BreakTime extends Model
 {
     use HasFactory;
+
+    protected $guarded = [
+        'id',
+    ];
+
+    public function user()
+    {
+    return $this->belongsTo(User::class);
+    }
+
+    public function attendance()
+    {
+    return $this->belongsTo(Attendance::class);
+    }
+
+    public function AttendanceRequestBreak()
+    {
+        return $this->hasMany(AttendanceRequestBreak::class);
+    }
+
 }
