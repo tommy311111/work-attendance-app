ff97110906b7d3f997f3dd112c6da91ae139705c
y_has <yuko1640@gmail.com>
2025-07-14 12:55:50 +0900

    Attendanceモデルの中身作成

--- src/app/Models/Attendance.php
+++ src/app/Models/Attendance.php
@@ -8,4 +8,23 @@ use Illuminate\Database\Eloquent\Model;
 class Attendance extends Model
 {
     use HasFactory;
+
+    protected $guarded = [
+        'id',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function breaks()
+    {
+    return $this->hasMany(BreakTime::class);
+    }
+
+    public function attendanceRequests()
+    {
+    return $this->hasMany(AttendanceRequest::class);
+    }
 }
