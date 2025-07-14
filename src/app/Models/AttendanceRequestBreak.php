ef8693f8114bb47268976e56b81ce1a24a25eed3
y_has <yuko1640@gmail.com>
2025-07-14 14:44:23 +0900

    AttendanceRequestBreakモデルの中身作成

--- src/app/Models/AttendanceRequestBreak.php
+++ src/app/Models/AttendanceRequestBreak.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class AttendanceRequestBreak extends Model
 {
     use HasFactory;
+
+    protected $guarded = [
+        'id',
+    ];
+
+    public function attendanceRequest()
+    {
+    return $this->belongsTo(Attendance::class);
+    }
+
+    public function BreakTimeRequest()
+    {
+    return $this->belongsTo(BreakTime::class);
+    }
 }
