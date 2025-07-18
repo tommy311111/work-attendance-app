89af4e143d899a87336804e8bf0e441909afcb83
y_has <yuko1640@gmail.com>
2025-07-18 13:11:12 +0900

    RegisterController.php作成

---
+++ src/app/Http/Controllers/RegisterController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+use App\Http\Requests\RegisterRequest;
+use App\Http\Controllers\Controller;
+use Illuminate\Auth\Events\Registered;
+
+class RegisterController extends Controller
+{
+    public function showRegistrationForm()
+    {
+        return view('auth.register');
+    }
+
+    public function register(RegisterRequest $request)
+    {
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        Auth::login($user);
+        event(new Registered($user));
+        return redirect()->route('profile.edit');//→ プロフィール編集画面に遷移する.今回は勤怠登録画面
+    }
+
+}
