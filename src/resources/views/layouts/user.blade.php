7f669d3f9228f2851fa5ceda79a4f7929c06950f
y_has <yuko1640@gmail.com>
2025-07-20 19:07:36 +0900

    ヘッダー修正

--- src/resources/views/layouts/user.blade.php
+++ src/resources/views/layouts/user.blade.php
@@ -10,34 +10,31 @@
 </head>
 <body>
 
-<header class="header">
-        
-                <h1 class="header__logo">
-                    <a href="{{ route('attendance.show') }}">
-                        <img src="{{ asset('storage/images/logo.svg') }}" alt="COACHTECH">
-                    </a>
-                </h1>
+    <header class="header">
+        <h1 class="header__logo">
+            <a href="{{ route('attendance.show') }}">
+                <img src="{{ asset('storage/images/logo.svg') }}" alt="COACHTECH">
+            </a>
+        </h1>
 
-            <nav class="header-nav">
-    <ul class="header-nav-list">
-      <li class="header-nav-item"><a href="">勤怠</a></li>
-      <li class="header-nav-item"><a href="">勤怠一覧</a></li>
-      <li class="header-nav-item"><a href="">申請</a></li>
-      <li class="header-nav-item">
-    <form action="/logout" method="POST" class="logout-form">
-        @csrf
-        <button type="submit" class="logout-button">ログアウト</button>
-    </form>
-</li>
-
-    </ul>
-  </nav>
-
-
-       
+        <nav class="header-nav">
+            <ul class="header-nav-list">
+                <li class="header-nav-item"><a href="">勤怠</a></li>
+                <li class="header-nav-item"><a href="">勤怠一覧</a></li>
+                <li class="header-nav-item"><a href="">申請</a></li>
+                <li class="header-nav-item">
+                    <form action="/logout" method="POST" class="logout-form">
+                        @csrf
+                        <button type="submit" class="logout-button">ログアウト</button>
+                    </form>
+                </li>
+            </ul>
+        </nav>
     </header>
+
     <main>
         @yield('content')
     </main>
+
 </body>
 </html>
