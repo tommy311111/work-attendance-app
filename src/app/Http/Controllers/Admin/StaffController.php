0744da208ec559e837a85292c13e753d449e3638
y_has <yuko1640@gmail.com>
2025-08-23 12:12:50 +0900

    StaffController品質改善

--- src/app/Http/Controllers/Admin/StaffController.php
+++ src/app/Http/Controllers/Admin/StaffController.php
@@ -2,19 +2,17 @@
 
 namespace App\Http\Controllers\Admin;
 
-use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 
 class StaffController extends Controller
 {
     public function index()
-{
-    $users = User::where('role', 'employee')
-                 ->with('attendanceRequests') // ← リクエストも一緒に取得
-                 ->get();
+    {
+        $users = User::where('role', 'employee')
+                    ->with('attendanceRequests')
+                    ->get();
 
-    return view('admin.staff.index', compact('users'));
-}
-
+        return view('admin.staff.index', compact('users'));
+    }
 }
